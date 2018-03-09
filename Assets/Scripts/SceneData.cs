7d51312db347424bbd6e7dcbf8d84580f9ef4944
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-03-09 15:31:56 -0500

    added submenu for selective training

---
+++ Assets/Scripts/SceneData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneData : MonoBehaviour {
+    private static string framing;
+
+    public void setFraming(string f) {
+        print("set framing" + f);
+        framing = f;
+    }
+
+    public string getFraming() {
+        return framing;
+    }
+}
