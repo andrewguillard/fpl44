d726d55888c740117351a5d9f3e3156bedf219a9
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-03-26 16:33:18 -0400

    damage generator is working , next is poledata implementation for CAF

---
+++ Assets/Scripts/PoleData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleData : MonoBehaviour {
+    public string[] equipment;
+    public int[] levelDamage;
+    
+	// Use this for initialization
+	void Start () {
+        //go to children objects and get data
+        initizalizeData(gameObject);
+	}
+
+    void initizalizeData(GameObject pole)
+    {
+        //go through each children 
+        foreach(transform i in pole.transform)
+        {
+
+        }
+    }
+}
