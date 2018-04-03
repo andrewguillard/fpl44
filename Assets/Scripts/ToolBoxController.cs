e7be42afd4f116a39fa0387f0bca6c44e89a4d0b
Cuong Nguyen <kevinnguyen0100@gmail.com>
2018-04-03 16:47:01 -0400

    added option menu

---
+++ Assets/Scripts/ToolBoxController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolBoxController : MonoBehaviour {
+    public GameObject compass;
+
+    // Use this for initialization
+    void Start () {
+        Button compassBtn = GameObject.Find("CompassBtn").GetComponent<Button>();
+        compassBtn.onClick.AddListener(CompassButton);
+	}
+
+    void CompassButton()
+    {
+        compass.gameObject.SetActive(!compass.gameObject.activeSelf);
+    }
+	
+    
+}
