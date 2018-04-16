b53b42f7ff761e9232007799ef7e2c4566002882
Julia Galimidi <juliagalimidi@yahoo.com>
2018-04-16 02:09:11 -0400

    Added in Quiz Screen prefab and script

---
+++ Assets/Scripts/QuizScoreScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuizScoreScript : MonoBehaviour
+{
+    public static Button SubmitQuizBtn;
+    public static Text ScoreTxt;
+    public int finalscoreInt = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get Components on the Quiz Screen
+        ScoreTxt = gameObject.transform.Find("ScoreTxt").GetComponent<Text>();
+        SubmitQuizBtn = gameObject.transform.Find("SubmitQuizBtn").GetComponent<Button>();
+    }
+
+    public void SubmitQuizBtn_OnClick()
+    {
+        //foreach ()
+        //{
+        finalscoreInt += ConditionAssessment.points4quiz;
+        //}
+
+        print("clicked");
+        string finalscoreString = finalscoreInt.ToString();
+        ScoreTxt.text = finalscoreString;
+    }
+}
